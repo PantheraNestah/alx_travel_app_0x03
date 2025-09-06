e679d656dc5f18d78dedd699854b0563edc78767
PantheraNestah <pantheranestah@gmail.com>
2025-09-06 16:40:06 +0300

    chore: Update settings.py to add panthera254.pythonanywhere.com to ALLOWED_HOSTS

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -35,6 +35,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://your-production-domain.com",
+    "panthera254.pythonanywhere.com"
 ]
 
 # Application definition
